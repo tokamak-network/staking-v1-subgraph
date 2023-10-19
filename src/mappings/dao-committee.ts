265fb8973313d2e7f962d383bacea1b5da4c2be5
cd4761 <cd4761@gmail.com>
2023-10-19 16:25:36 +0900

    update schema

---
+++ src/mappings/dao-committee.ts
@@ -0,0 +1,31 @@
+import { Factory, Candidate } from '../../generated/schema'
+import { ZERO_BI, ONE_BI, ZERO_BD, ADDRESS_ZERO } from '../../utils/constants'
+import { CandidateContractCreated } from '../../generated/DAOCommittee/DAOCommittee';
+
+export function handleCandidateContractCreated (event: CandidateContractCreated): void {
+  let factory = Factory.load('1')
+  if (factory == null) {
+    factory = new Factory('1')
+
+    factory.totalStaked = ZERO_BI
+    factory.totalPendingWithdrawal = ZERO_BI
+    factory.numOfCandidate = ZERO_BI
+  }
+  factory.numOfCandidate = factory.numOfCandidate.plus(ONE_BI)
+  // factory.factoryCandidateCount = factory.factoryCandidateCount.plus(ONE_BI)
+
+  let candidate = new Candidate(event.params.candidateContract) as Candidate
+  
+  candidate.id = event.params.candidateContract
+  candidate.candidate = event.params.candidate
+  candidate.candidateContract = event.params.candidateContract
+  candidate.name = event.params.memo.toString()
+  candidate.commissionRate = ZERO_BD
+  
+  // candidate.txCount = ZERO_BI
+  candidate.stakedAmount = ZERO_BI
+  candidate.pendingWithdrawalAmount = ZERO_BI
+
+  candidate.save()
+  factory.save()
+}
